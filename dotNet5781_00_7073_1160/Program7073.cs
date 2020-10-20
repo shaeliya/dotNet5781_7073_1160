8d7a5b4e9adae9e2a455795caa7db983cc384b16
Orit Stavsky <orstavsk@g.jct.ac.il>
2020-10-20 22:22:11 +0300

    Adding a class Wellcome7073

---
+++ dotNet5781_00_7073_1160/Program7073.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotNet5781_00_7073_1160
+{
+    partial class Program
+    {
+        static partial void Wellcome7073()
+        {
+            Console.WriteLine("The time is 22:17 20/10/2020");
+        }
+       
+    }
+}
