48bdeb9b81687aa15b5e9335f02df5f8729f27ef
shalhevet2001@gmail.com <shalhevet2001@gmail.com>
2020-10-20 22:06:45 +0300

    initial code + Wellcome1160

---
+++ dotNet5781_00_7073_1160/Program1160.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_7073_1160
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Wellcome1160();
+            Wellcome7073();
+
+            Console.ReadKey();
+        }
+        static partial void Wellcome7073();
+        private static void Wellcome1160()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
