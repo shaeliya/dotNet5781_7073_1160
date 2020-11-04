6437e2a15971bf58cc25d2a1fffb6a22650b0402
shalhevet2001@gmail.com <shalhevet2001@gmail.com>
2020-11-04 12:11:56 +0200

    Creating a new project, creating a new class, and creating fields for the class

---
+++ dotNet5781_02_7073_1160/BusStop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_7073_1160
+{
+    class BusStop
+    {
+        string StationCode; // קוד תחנה
+        double Latitude; //קו רוחב
+        double Longitude; //קו אורך
+    }
+}
---
+++ dotNet5781_02_7073_1160/main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_7073_1160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
