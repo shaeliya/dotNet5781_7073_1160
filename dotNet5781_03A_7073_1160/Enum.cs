bad87c32dca07f8266311c952d9e6aa2243f9f56
shalhevet2001@gmail.com <shalhevet2001@gmail.com>
2020-11-23 20:42:20 +0200

    step 4-5

---
+++ dotNet5781_03A_7073_1160/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_7073_1160
+{
+    public class Enum
+   {
+        public enum Area
+        {
+            General,
+            North,
+            South,
+            Center,
+            Jerusalem,
+            TelAviv,
+            YehudaShomron,
+            Haifa
+        }
+    }
+}
