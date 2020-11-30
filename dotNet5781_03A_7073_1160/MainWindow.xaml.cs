8db9f60cdad945208ec190c10d5cd6a384be8b9c
shalhevet2001@gmail.com <shalhevet2001@gmail.com>
2020-11-30 09:45:52 +0200

    Ex03A Final commit

--- dotNet5781_03A_7073_1160/MainWindow.xaml.cs
+++ dotNet5781_03A_7073_1160/MainWindow.xaml.cs
@@ -1,27 +1,19 @@
-﻿//Shalhevet Eliyahu 211661160
-//Orit Stavsky 212507073
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using dotNet5781_02_7073_1160;
-
+//----------------------------
+//----------------------------
+//Shalhevet Eliyahu 211661160
+//Orit Stavsky 212507073
+//----------------------------
+//----------------------------
 namespace dotNet5781_03A_7073_1160
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+public partial class MainWindow : Window
     {
         public BusCollection busLines;
         public BusLine currentDisplayBusLine;
