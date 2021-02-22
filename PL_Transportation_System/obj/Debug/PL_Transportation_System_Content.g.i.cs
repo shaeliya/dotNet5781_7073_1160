272b9f89f9dbeecde2ba020cb723520bf5f87104
אורית סטבסקי <orstavsk@g.jct.ac.il>
2021-02-22 17:47:38 +0200

    add image

--- PL_Transportation_System/obj/Debug/PL_Transportation_System_Content.g.i.cs
+++ PL_Transportation_System/obj/Debug/PL_Transportation_System_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sources/delete.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("delete.png")]
 
 
